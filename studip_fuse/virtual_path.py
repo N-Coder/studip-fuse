N-Coder/studip-fuse

commit b945f2d8e0e7c1da3fb6162b3bcc454e28cd8d92

fixed support for filenames with slashes

studip_fuse/virtual_path.py
@@ -200,8 +200,7 @@ class VirtualPath(object):
             })
 
         if self._file:
-            # FIXME 2017WS/StudIP Sync Testers/Studien--Arbeitsgruppe/Arbeitsgruppe- StudIP Sync Testers/Allgemeiner Dateiordner/
-            path = short_path = self._file.path.split("/")[2:-1]  # ''/'1234VL-Name'/[path]/'file_name'
+            path = short_path = self._file.path[2:-1]  # ''/'1234VL-Name'/[path]/'file_name'
             if short_path[0:1] == ["Allgemeiner Dateiordner"]:
                 short_path = path_tail(short_path)
 

commit bebc42c4f3c059f50d196d734abaa0c28d8f36bd

fixed bug introduced in b945f2d, crashing if course didn't use "Allgemeiner Dateiordner"

studip_fuse/virtual_path.py
@@ -200,7 +200,7 @@ class VirtualPath(object):
             })
 
         if self._file:
-            path = short_path = self._file.path[2:-1]  # ''/'1234VL-Name'/[path]/'file_name'
+            path = short_path = self._file.path[1:-1]  # '1234VL-Name'/[path]/'file_name'
             if short_path[0:1] == ["Allgemeiner Dateiordner"]:
                 short_path = path_tail(short_path)
 