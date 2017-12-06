c3204398386f2865de6c3b6c3021438f6a665651
Niko Fink <nfink95@gmail.com>
2017-12-06 11:57:00 +0100

    improve support for calling via mount

--- studip_fuse/__main__.py
+++ studip_fuse/__main__.py
@@ -1,32 +1,27 @@
-import argparse as argparse
 import asyncio
 import concurrent.futures
 import functools
 import logging
 import os
 import sys
-import threading
-import traceback
 import warnings
 from getpass import getpass
 from threading import Thread
 
-import appdirs
 import sh
-from fuse import FUSE
-from more_itertools import flatten, one
+from fuse import FUSE, fuse_get_context
 
 from studip_fuse.cached_session import CachedStudIPSession
+from studip_fuse.cmd_util import dump_loop_stack, format_stack, parse_args, thread_log
 from studip_fuse.fs_driver import FUSEView, LoggingFUSEView
 from studip_fuse.real_path import RealPath
 from studip_fuse.virtual_path import VirtualPath
 
 logging.basicConfig(level=logging.INFO)
-thread_log = logging.getLogger("threads")
 
 
 def main():
-    args = parse_args()
+    args, http_args, fuse_args = parse_args()
     if args.debug:
         logging.root.setLevel(logging.DEBUG)
         warnings.resetwarnings()
@@ -35,14 +30,15 @@ def main():
         logging.getLogger("asyncio").setLevel(logging.WARNING)
 
     future = concurrent.futures.Future()
-    loop_thread = Thread(target=functools.partial(run_loop, args, future), name="aio event loop", daemon=True)
+    loop_thread = Thread(target=functools.partial(run_loop, args, http_args, future), name="aio event loop",
+                         daemon=True)
     loop_thread.start()
     loop, session = None, None
     try:
         logging.debug("Loop thread started, waiting for session initialization")
         loop, session = future.result()
 
-        run_fuse(loop, session, args)
+        run_fuse(loop, session, args, fuse_args)
     except:
         logging.error("FUSE driver crashed", exc_info=True)
     finally:
@@ -51,58 +47,10 @@ def main():
         if loop:
             loop.stop()
 
-        # print loop stack trace until the loop thread completed
-        counter = 0
-        while loop_thread.is_alive() and counter < 6:
-            loop_thread.join(5)
-            counter += 1
-            if loop_thread.is_alive():
-                if loop:
-                    dump_loop_stack(loop)
-                else:
-                    thread_log.info("Waiting for loop thread to abort initialization...")
-                    thread_log.debug("Thread stack trace:\n %s", format_stack(sys._current_frames()[loop_thread.ident]))
-
-        if loop_thread.is_alive():
-            logging.warning("Shutting down main thread and thus killing hung event loop daemon thread")
+        await_loop_thread_shutdown(loop, loop_thread)
 
 
-class StoreNameValuePair(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        values = flatten(v.split(',') for v in values)
-        for value in values:
-            if '=' in value:
-                n, v = value.split('=')
-                setattr(namespace, n, v)
-            elif value.startswith("no"):
-                setattr(namespace, value[2:], False)
-            else:
-                setattr(namespace, value, True)
-
-
-def parse_args():
-    from studip_fuse import __version__ as prog_version, __author__ as prog_author
-    dirs = appdirs.AppDirs("Stud.IP-Fuse", prog_author)
-    parser = argparse.ArgumentParser(description='Stud.IP Fuse', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('user', help='Stud.IP username')
-    parser.add_argument('mount', help='path to mount point')
-    parser.add_argument('--pwfile', help='path to password file or "-" to read from stdin',
-                        default=os.path.join(dirs.user_config_dir, '.studip-pw'))
-    parser.add_argument('--format', help='format specifier for virtual paths',
-                        default="{semester-lexical-short}/{course}/{type}/{short-path}/{name}")
-    parser.add_argument('--cache', help='path to cache directory', default=dirs.user_cache_dir)
-    parser.add_argument('--studip', help='Stud.IP base URL', default="https://studip.uni-passau.de")
-    parser.add_argument('--sso', help='SSO base URL', default="https://sso.uni-passau.de")
-    parser.add_argument('--debug', help='enable debug mode', action='store_true')
-    parser.add_argument('--allowroot', help='allow root to access the mounted directory',
-                        action='store_true')
-    parser.add_argument('--version', action='version', version="%(prog)s " + prog_version)
-    parser.add_argument('-o', help='FUSE-like options', nargs='+', action=StoreNameValuePair)
-    args = parser.parse_args()
-    return args
-
-
-def run_loop(args, future: concurrent.futures.Future):
+def run_loop(args, http_args, future: concurrent.futures.Future):
     try:
         logging.info("Initializing asyncio event loop...")
         loop = asyncio.new_event_loop()
@@ -113,58 +61,63 @@ def run_loop(args, future: concurrent.futures.Future):
         logging.debug("Loop initialization failed, propagating result back to main thread")
         future.set_exception(e)
         raise
-    if future.cancelled():
-        return
 
+    session = None
     try:
-        try:
-            logging.info("Opening StudIP session")
-            if args.pwfile == "-":
-                password = getpass()
-            else:
-                try:
-                    with open(args.pwfile) as f:
-                        password = f.read()
-                except FileNotFoundError as e:
-                    logging.warning("%s. Either specifiy a file from which your Stud.IP password can be read "
-                                    "or use `--pwfile -` to enter it using a promt in the shell." % e)
-                    future.set_exception(e)
-                    return
-            coro = CachedStudIPSession(
-                user_name=args.user, password=password.strip(),
-                studip_base=args.studip, sso_base=args.sso,
-                cache_dir=args.cache
-            ).__aenter__()
-            password = ""
-            task = asyncio.ensure_future(coro, loop=loop)
-            future.add_done_callback(lambda f: task.cancel() if f.cancelled() else None)
-            if future.cancelled():
+        if future.cancelled():
+            return
+
+        logging.info("Opening StudIP session")
+        if args.pwfile == "-":
+            password = getpass()
+        else:
+            try:
+                with open(args.pwfile) as f:
+                    password = f.read()
+            except FileNotFoundError as e:
+                logging.warning("%s. Either specifiy a file from which your Stud.IP password can be read "
+                                "or use `--pwfile -` to enter it using a promt in the shell." % e)
+                future.set_exception(e)
                 return
-            session = loop.run_until_complete(task)
-        except Exception as e:
-            logging.debug("Session initialization failed, propagating result back to main thread")
-            future.set_exception(e)
-            raise
+        coro = CachedStudIPSession(
+            user_name=args.user, password=password.strip(),
+            studip_base=args.studip, sso_base=args.sso,
+            cache_dir=args.cache, http_args=http_args
+        ).__aenter__()
+        password = ""
+        task = asyncio.ensure_future(coro, loop=loop)
+        future.add_done_callback(lambda f: task.cancel() if f.cancelled() else None)
+        if future.cancelled():
+            return
+        session = loop.run_until_complete(task)
         if future.cancelled():
             return
 
         logging.debug("Loop and session ready, sending result back to main thread")
         future.set_result((loop, session))
 
-        try:
-            logging.info("Running asyncio event loop...")
-            loop.run_forever()
-        finally:
-            logging.info("asyncio event loop stopped, cleaning up")
-            try:
-                loop.run_until_complete(shutdown_loop_async(loop, session))
-                logging.info("Cleaned up, closing event loop")
-            except:
-                logging.warning("Clean-up failed, closing", exc_info=True)
+        logging.info("Running asyncio event loop...")
+        loop.run_forever()
+
+    except Exception as e:
+        if not future.done():
+            future.set_exception(e)
 
     finally:
+        logging.info("asyncio event loop stopped, cleaning up and closing")
+
         if not future.done():
-            logging.warning("Event loop thread did not report result back to main thread, will probably hang.")
+            msg = "Event loop thread did not report result back to main thread."
+            logging.warning(msg)
+            future.set_exception(RuntimeError(msg))
+
+        try:
+            if session:
+                loop.run_until_complete(shutdown_loop_async(loop, session))
+                logging.info("Cleaned up session, closing event loop")
+        except:
+            logging.warning("Clean-up failed, closing", exc_info=True)
+
         loop.close()
         logging.info("Event loop closed, shutdown complete")
 
@@ -177,7 +130,24 @@ async def shutdown_loop_async(loop, session):
     logging.debug("Loop drained")
 
 
-def run_fuse(loop, session, args):
+def await_loop_thread_shutdown(loop, loop_thread):
+    # print loop stack trace until the loop thread completed
+    counter = 0
+    while loop_thread.is_alive() and counter < 6:
+        loop_thread.join(5)
+        counter += 1
+        if loop_thread.is_alive():
+            if loop:
+                dump_loop_stack(loop)
+            else:
+                thread_log.info("Waiting for loop thread to abort initialization...")
+                thread_log.debug("Thread stack trace:\n %s", format_stack(sys._current_frames()[loop_thread.ident]))
+
+    if loop_thread.is_alive():
+        logging.warning("Shutting down main thread and thus killing hung event loop daemon thread")
+
+
+def run_fuse(loop, session, args, fuse_args):
     logging.info("Initializing virtual file system")
     vp = VirtualPath(session=session, path_segments=[], known_data={}, parent=None,
                      next_path_segments=args.format.split("/"))
@@ -194,45 +164,19 @@ def run_fuse(loop, session, args):
         if "entry for" not in str(e) or "not found in" not in str(e):
             logging.warning("Could not unmount mount path %s", args.mount, exc_info=True)
         else:
+            # print last line of stderr output, containing the entry not found message
             logging.debug(e.stderr.decode("UTF-8", "replace").strip().split("\n")[-1])
+    except sh.CommandNotFound as e:
+        logging.info("Could not unmount mountpoint before mounting, because fusermount is not available")
 
-    logging.debug("Initialization done, handing over to FUSE driver")
+    logging.debug("Initialization done, handing over to FUSE driver to mount at %s (uid=%s, gid=%s, pid=%s)",
+                  args.mount, *fuse_get_context())
     if args.debug:
         fuse_ops = LoggingFUSEView(rp, loop)
     else:
         fuse_ops = FUSEView(rp, loop)
     logging.info("Ready")
-    FUSE(fuse_ops, args.mount, foreground=True, allow_root=args.allowroot, debug=args.debug)
-
-
-def dump_loop_stack(loop):
-    current_task = asyncio.Task.current_task(loop=loop)
-    pending_tasks = [t for t in asyncio.Task.all_tasks(loop=loop) if not t.done() and t is not current_task]
-    loop_thread = one(t for t in threading.enumerate() if t.ident == loop._thread_id)
-    loop_thread_stack = sys._current_frames()[loop_thread.ident]
-    loop_thread_stack_trace = format_stack(loop_thread_stack)
-
-    if thread_log.isEnabledFor(logging.DEBUG):
-        thread_log.debug("Current task %s in loop %s in loop thread %s", current_task, loop, loop_thread)
-        if current_task:
-            thread_log.debug("Task stack trace:\n %s", format_stack(current_task.get_stack()))
-        thread_log.debug("Thread stack trace:\n %s", loop_thread_stack_trace)
-        pending_tasks_str = "\n".join(
-            str(t) + "\n" + format_stack(t.get_stack())
-            for t in pending_tasks)
-        thread_log.debug("%s further pending tasks:\n %s", len(pending_tasks), pending_tasks_str)
-    else:
-        thread_log.info("Waiting for event loop to stop... (%s further pending tasks after current task %s "
-                        "in loop %s in loop thread %s)", len(pending_tasks), current_task, loop, loop_thread)
-
-    if len(pending_tasks) == 0 and current_task is None \
-            and "epoll.poll(timeout, max_ev)" in loop_thread_stack_trace.strip().split("\n")[-1]:
-        thread_log.warning("Event loop hangs in epoll selector without any tasks pending. "
-                           "This is probably a python bug (https://bugs.python.org/issue29780).")
-
-
-def format_stack(stack):
-    return "".join(traceback.format_stack(stack[0] if isinstance(stack, list) else stack))
+    FUSE(fuse_ops, args.mount, **fuse_args)
 
 
 if __name__ == "__main__":
---
+++ studip_fuse/cmd_util.py
@@ -0,0 +1,112 @@
+import argparse as argparse
+import asyncio
+import logging
+import os
+import sys
+import threading
+import traceback
+
+import appdirs
+from more_itertools import flatten, one
+
+thread_log = logging.getLogger("threads")
+
+
+def StoreNameValuePair(option_parser):
+    class anonymous_class(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            ignored_values = []
+            values = flatten(v.split(',') for v in values)
+            for value in values:
+                if value in ["suid", "nosuid", "dev", "nodev", "ro"]:
+                    ignored_values.append(value)
+                elif value == "rw":
+                    parser.error("Stud.IP FUSE only supports read-only mount")
+                else:
+                    option_parser.parse_args(["--" + value], namespace)
+            if ignored_values:
+                logging.warning("Ignoring arguments %s" % ", ".join(ignored_values))
+
+    return anonymous_class
+
+
+def parse_args():
+    from studip_fuse import __version__ as prog_version, __author__ as prog_author
+    dirs = appdirs.AppDirs("Stud.IP-Fuse", prog_author)
+
+    opts_parser = argparse.ArgumentParser(add_help=False)
+    opts_parser.add_argument("-o", help="FUSE-like options", nargs="+", action=StoreNameValuePair(opts_parser))
+    debug = opts_parser.add_argument("-d", "--debug", help="enable debug mode", action="store_true")
+
+    studip_opts = opts_parser.add_argument_group("Stud.IP Driver Options")
+    studip_opts.add_argument("--pwfile", help="path to password file or '-' to read from stdin",
+                             default=os.path.join(dirs.user_config_dir, ".studip-pw"))
+    studip_opts.add_argument("--format", help="format specifier for virtual paths",
+                             default="{semester-lexical-short}/{course}/{type}/{short-path}/{name}")
+    studip_opts.add_argument("--cache", help="path to cache directory", default=dirs.user_cache_dir)
+    studip_opts.add_argument("--studip", help="Stud.IP base URL", default="https://studip.uni-passau.de")
+    studip_opts.add_argument("--sso", help="SSO base URL", default="https://sso.uni-passau.de")
+
+    fuse_opts = opts_parser.add_argument_group("FUSE Options")
+    fuse_opts.add_argument("--foreground", help="run in foreground", action="store_true")
+    fuse_opts.add_argument("--allow_other", help="allow access by all users", action="store_true")
+    fuse_opts.add_argument("--allow_root", help="allow access by root", action="store_true")
+    fuse_opts.add_argument("--nonempty", help="allow mounts over non-empty file/dir", action="store_true")
+    fuse_opts.add_argument("--umask", help="set file permissions (octal)", action="store")
+    fuse_opts.add_argument("--uid", help="set file owner", action="store")
+    fuse_opts.add_argument("--gid", help="set file group", action="store")
+    fuse_opts.add_argument("--default_permissions", help="enable permission checking by kernel",
+                           action="store_true")
+
+    http_opts = opts_parser.add_argument_group("HTTP Client Options")
+    http_opts.add_argument("--read_timeout", action="store", help="request operations timeout in seconds", default=30,
+                           type=float)
+    http_opts.add_argument("--conn_timeout", action="store", help="timeout for connection establishing in seconds",
+                           default=30, type=float)
+    http_opts.add_argument("--keepalive_timeout", action="store",
+                           help="timeout for connection reusing after releasing in seconds", default=60, type=float)
+    http_opts.add_argument("--limit", action="store", help="total number simultaneous connections", default=10,
+                           type=int)
+    http_opts.add_argument("--force_close", action="store_true", help="disable HTTP keep-alive")
+
+    parser = argparse.ArgumentParser(description="Stud.IP Fuse", formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     parents=[opts_parser])
+    parser.add_argument("user", help="Stud.IP username")
+    parser.add_argument("mount", help="path to mount point")
+    parser.add_argument("-V", "--version", action="version", version="%(prog)s " + prog_version)
+
+    args = parser.parse_args()
+    http_args = {a.dest: getattr(args, a.dest, None) for a in http_opts._group_actions}
+    fuse_args = {a.dest: getattr(args, a.dest, None) for a in fuse_opts._group_actions + [debug]
+                 if getattr(args, a.dest, None) is not None}
+    return args, http_args, fuse_args
+
+
+def dump_loop_stack(loop):
+    current_task = asyncio.Task.current_task(loop=loop)
+    pending_tasks = [t for t in asyncio.Task.all_tasks(loop=loop) if not t.done() and t is not current_task]
+    loop_thread = one(t for t in threading.enumerate() if t.ident == loop._thread_id)
+    loop_thread_stack = sys._current_frames()[loop_thread.ident]
+    loop_thread_stack_trace = format_stack(loop_thread_stack)
+
+    if thread_log.isEnabledFor(logging.DEBUG):
+        thread_log.debug("Current task %s in loop %s in loop thread %s", current_task, loop, loop_thread)
+        if current_task:
+            thread_log.debug("Task stack trace:\n %s", format_stack(current_task.get_stack()))
+        thread_log.debug("Thread stack trace:\n %s", loop_thread_stack_trace)
+        pending_tasks_str = "\n".join(
+            str(t) + "\n" + format_stack(t.get_stack())
+            for t in pending_tasks)
+        thread_log.debug("%s further pending tasks:\n %s", len(pending_tasks), pending_tasks_str)
+    else:
+        thread_log.info("Waiting for event loop to stop... (%s further pending tasks after current task %s "
+                        "in loop %s in loop thread %s)", len(pending_tasks), current_task, loop, loop_thread)
+
+    if len(pending_tasks) == 0 and current_task is None \
+            and "epoll.poll(timeout, max_ev)" in loop_thread_stack_trace.strip().split("\n")[-1]:
+        thread_log.warning("Event loop hangs in epoll selector without any tasks pending. "
+                           "This is probably a python bug (https://bugs.python.org/issue29780).")
+
+
+def format_stack(stack):
+    return "".join(traceback.format_stack(stack[0] if isinstance(stack, list) else stack))
