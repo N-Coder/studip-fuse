60d6b069da3cd6fcfacef2eab9fe14a09dcee107
Niko Fink <nfink95@gmail.com>
2018-02-23 14:15:42 +0100

    small clean-ups on wrong *-exports and forgotten setup required packages

--- studip_fuse/cache/__init__.py
+++ studip_fuse/cache/__init__.py
@@ -1,4 +1,4 @@
 from studip_fuse.cache.async_cache import *
-from studip_fuse.cache.cached_session import CachedStudIPSession, cached_download_validator
+from studip_fuse.cache.cached_session import CachedStudIPSession
 
-__all__ = async_cache.__all__ + ["CachedStudIPSession", "cached_download_validator"]
+__all__ = async_cache.__all__ + ["CachedStudIPSession"]
