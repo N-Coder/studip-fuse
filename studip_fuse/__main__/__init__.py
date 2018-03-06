3753f21519e2abea09e6278dcf2fd206f7f131cf
Niko Fink <nfink95@gmail.com>
2018-03-06 18:04:09 +0100

    Windows and Mac OS compatibility

--- studip_fuse/__main__/__init__.py
+++ studip_fuse/__main__/__init__.py
@@ -1,12 +1,15 @@
 import faulthandler
 import logging
 import logging.config
+import os
 import sys
 import threading
 
+import appdirs
 import pkg_resources
 import yaml
 
+from studip_fuse import __author__ as prog_author
 from studip_fuse.__main__.cmd_util import parse_args
 from studip_fuse.__main__.fs_driver import FixedFUSE
 
@@ -42,14 +45,46 @@ class LoggerWriter:
             self.old.flush()
 
 
+def configure_logging():
+    dirs = appdirs.AppDirs("Stud.IP-Fuse", prog_author)
+    os.makedirs(dirs.user_data_dir, exist_ok=True)
+
+    logging_path = os.path.join(dirs.user_config_dir, "studip-logging-config.yaml")
+    if os.path.isfile(logging_path):
+        with open(logging_path, "rb") as f:
+            logging_config = yaml.load(f)
+    else:
+        logging_config = yaml.load(pkg_resources.resource_string("studip_fuse.__main__", "logging.yaml"))
+
+    if "handlers" in logging_config:
+        handlers = logging_config["handlers"]
+
+        if "syslog" in handlers and "address" not in handlers["syslog"]:
+            if os.path.exists("/dev/log"):
+                # probably Linux syslog
+                handlers["syslog"]["address"] = "/dev/log"
+            elif os.path.exists("/var/run/syslog"):
+                # probably Mac OS syslog
+                handlers["syslog"]["address"] = "/var/run/syslog"
+            else:
+                # syslog not available (e.g. Windows), disable
+                handlers["syslog"] = {"class": "logging.NullHandler"}
+
+        if "file" in handlers and "filename" not in handlers["file"]:
+            handlers["file"]["filename"] = os.path.join(dirs.user_data_dir, "studip-log.txt")
+
+    logging.config.dictConfig(logging_config)
+
+    sys.excepthook = excepthook
+    faulthandler.enable(file=open(os.path.join(dirs.user_data_dir, "studip-fault-tb.txt"), "wt"), all_threads=True)
+    # reroute std streams after logging config, so that a config logging to sys.stdout still logs to the initial stream
+    sys.stdout = LoggerWriter(logging.getLogger('studip_fuse.stdout').info, sys.stdout)
+    sys.stderr = LoggerWriter(logging.getLogger('studip_fuse.stderr').error, sys.stderr)
+
+
 def main():
     try:
-        logging.config.dictConfig(yaml.load(pkg_resources.resource_string('studip_fuse.__main__', 'logging.yaml')))
-        sys.excepthook = excepthook
-        faulthandler.enable(file=sys.stderr, all_threads=True)
-        # reroute std streams after logging config, so that a config logging to sys.stdout still logs to the initial stream
-        sys.stdout = LoggerWriter(logging.getLogger('studip_fuse.stdout').info, sys.stdout)
-        sys.stderr = LoggerWriter(logging.getLogger('studip_fuse.stderr').error, sys.stderr)
+        configure_logging()
 
         args, http_args, fuse_args = parse_args()
 
@@ -59,7 +94,6 @@ def main():
             logging.getLogger("asyncio").setLevel(logging.WARNING)
         logging.debug("Program started")
 
-        import os
         os.makedirs(args.cache, exist_ok=True)
 
         from studip_fuse.__main__.fs_driver import FUSEView
