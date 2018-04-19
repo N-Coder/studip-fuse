0e96c8905ea2e0a9db68fd1f3b92de8e5c1b83c5
Niko Fink <nfink95@gmail.com>
2018-04-19 09:10:20 +0200

    complete rewrite of async cache implementation

--- studip_fuse/cache/cached_session.py
+++ studip_fuse/cache/cached_session.py
@@ -1,122 +1,118 @@
-import json
+import functools
 import os
 import time
 from stat import S_ISREG
+from typing import Any, Dict
 
 import aiofiles.os as aio_os
 import attr
+import cattr
+from attr import Factory
 
 from studip_api.async_delay import DeferredTask, DelayLatch, await_idle
 from studip_api.downloader import Download
-from studip_api.model import *
+from studip_api.model import Course, File, ModelClass, Semester, register_datetime_converter, register_forwardref_converter, register_model_converter
 from studip_api.session import StudIPSession, log
-from studip_fuse.cache import DownloadTaskCache, ModelGetterCache, cached_task
+from studip_fuse.cache.async_cache import AsyncDownloadCache, AsyncModelCache
 from studip_fuse.cache.circuit_breaker import NetworkCircuitBreaker
 
+CACHED_GETTERS = ("get_semesters", "get_courses", "get_course_root_file", "get_folder_files")
+TYPE_REGISTRY = {"Semester": Semester, "Course": Course, "File": File}
 
-@attr.s(hash=False)
+
+@attr.s()
+class SessionModelRepo(object):
+    semesters = attr.ib(type=Dict[str, Semester], default=Factory(dict))
+    courses = attr.ib(type=Dict[str, Course], default=Factory(dict))
+    files = attr.ib(type=Dict[str, File], default=Factory(dict))
+
+    def get_type_dict(self, t):
+        return {Semester: self.semesters, Course: self.courses, File: self.files}[t]
+
+    def get(self, _type, _id):
+        return self.get_type_dict(_type)[_id]
+
+    def put(self, _type, _value):
+        self.get_type_dict(_type)[_value.id] = _value
+
+
+@attr.s(hash=False, str=False, repr=False)
 class CachedStudIPSession(StudIPSession):
-    cache_dir = attr.ib()  # type: str
+    cache_dir = attr.ib(default=None, validator=attr.validators.instance_of(str))  # type: str
+    circuit_breaker = attr.ib(default=Factory(NetworkCircuitBreaker))  # type: NetworkCircuitBreaker
+
+    _persist_caches_task = attr.ib(init=False, repr=False, default=None)  # type: DeferredTask
+    _model_data_repo = attr.ib(init=False, repr=False, default=Factory(SessionModelRepo))  # type: SessionModelRepo
 
     def __attrs_post_init__(self):
-        self.circuit_breaker = NetworkCircuitBreaker()
-        self._http_args["trace_configs"] = [self.circuit_breaker.trace_config]
-
-        super().__attrs_post_init__()
-
         self._persist_caches_task = DeferredTask(
             run=self.save_model, trigger_latch=DelayLatch(sleep_fun=await_idle), trigger_delay=60 * 5)
 
-        self.parser.SemesterFactory = Semester.get_or_create
-        self.parser.CourseFactory = Course.get_or_create
-        self.parser.FileFactory = File.get_or_create
-        self.parser.FolderFactory = Folder.get_or_create
+        base_converter = lambda: register_forwardref_converter(register_datetime_converter(cattr.Converter()))
+        model_ref_converter = lambda: register_model_converter(
+            base_converter(), base_converter(), self.structure_model_class)
+        # self.model_converter, ref_converter = model_ref_converter()
+        # self.cache_converter = register_cachevalue_converter(
+        #     model_ref_converter()[1], type_registry=TYPE_REGISTRY)
+        self.parser.converter, _ = model_ref_converter()
 
-        for func in (self.get_semesters, self.get_courses, self.get_course_files, self.get_folder_files):
-            func._may_create = self.circuit_breaker.may_create
-            func._exception_handler = self.circuit_breaker.exception_handler
+        super().__attrs_post_init__()
+
+        for getter in CACHED_GETTERS:
+            wrapped = getattr(self, getter)
+            wrapper = AsyncModelCache(
+                wrapped_function=wrapped,
+                may_attempt=lambda _, __: self.circuit_breaker.allow_request(),
+                on_new_value_fetched=lambda _: self._persist_caches_task.defer()
+            )
+            functools.update_wrapper(wrapper, wrapped)
+            setattr(self, getter, wrapper)
+        self.download_file_contents = AsyncDownloadCache(
+            self.download_file_contents,
+            may_attempt=lambda _, __: self.circuit_breaker.allow_request()
+        )
 
     async def close(self):
         await self._persist_caches_task.finalize()
         await super().close()
 
+    def structure_model_class(self, mc_data: Any, t) -> ModelClass:
+        if isinstance(mc_data, t):
+            return mc_data
+        elif isinstance(mc_data, str):
+            return self._model_data_repo.get(t, mc_data)
+        else:
+            obj = self.parser.converter.structure_attrs_fromdict(mc_data, t)
+            self._model_data_repo.put(t, obj)
+            return obj
+
     async def save_model(self, path=None):
         if not path:
             path = os.path.join(self.cache_dir, "model_data.json")
 
-        # this is an async function, preventing all other async functions from modifying data while generating the obj
-        start = time.perf_counter()
-        data = ModelObjectMeta.export_all_data()
-        for func in (self.get_semesters, self.get_courses, self.get_course_files, self.get_folder_files):
-            data[func.__name__] = func.export_cache()
-        delta = time.perf_counter() - start
-
-        def save(obj_data):
-            with open(path, "wt") as f:
-                json.dump(obj_data, f)
-
-        await self._loop.run_in_executor(None, save, data)
-
-        log.info("Saved model and cache data, export took %ss", delta)
-        return "stored, took %ss" % delta
+        # TODO implement
+        # def save(obj_data):
+        #     with open(path, "wt") as f:
+        #         json.dump(obj_data, f)
+        #
+        # data = {"model": self.model_converter.unstructure(self._model_data_repo)}
+        # for getter in CACHED_GETTERS:
+        #     data[getter] = self.cache_converter.unstructure(getattr(self, getter).cache)
+        # await self.loop.run_in_executor(None, save, data)
 
     async def load_model(self, update=False, path=None):
         if not path:
             path = os.path.join(self.cache_dir, "model_data.json")
 
-        def load():
-            with open(path, "rt") as f:
-                return json.load(f)
+        # def load():
+        #     with open(path, "rt") as f:
+        #         return json.load(f)
+        #
+        # data = await self.loop.run_in_executor(None, load)
+        # self._model_data_repo.update(self.model_converter.structure(data["model"], SessionModelRepo))
+        # for getter in CACHED_GETTERS:
+        #     getattr(self, getter).update(self.cache_converter.structure(data[getter], AbstractCache))
 
-        data = await self._loop.run_in_executor(None, load)
-
-        # this is an async function, preventing all other async functions from modifying data while loading the obj
-        start = time.perf_counter()
-        func_imports = [(func, data.pop(func.__name__)) for func in
-                        (self.get_semesters, self.get_courses, self.get_course_files, self.get_folder_files)]
-        ModelObjectMeta.import_all_data(data, update)
-        for func, fun_data in func_imports:
-            func.import_cache(fun_data, update, create_future=self._loop.create_future)
-        delta = time.perf_counter() - start
-
-        log.info("Loaded model and cache data, import took %ss", delta)
-        return "loaded, took %ss" % delta
-
-    def model_cache_stats(self):
-        return {
-            "known_semesters": len(Semester.INSTANCES),
-            "known_courses": len(Course.INSTANCES),
-            "known_files": len(File.INSTANCES),
-            "indexed_semesters": len(self.get_courses._cache),
-            "indexed_courses": len(self.get_course_files._cache),
-            "indexed_folders": len(self.get_folder_files._cache)
-        }
-
-    @cached_task(cache_class=ModelGetterCache)
-    async def get_semesters(self):
-        res = await super().get_semesters()
-        self._persist_caches_task.defer()
-        return res
-
-    @cached_task(cache_class=ModelGetterCache)
-    async def get_courses(self, semester):
-        res = await super().get_courses(semester)
-        self._persist_caches_task.defer()
-        return res
-
-    @cached_task(cache_class=ModelGetterCache)
-    async def get_course_files(self, course):
-        res = await super().get_course_files(course)
-        self._persist_caches_task.defer()
-        return res
-
-    @cached_task(cache_class=ModelGetterCache)
-    async def get_folder_files(self, folder):
-        res = await super().get_folder_files(folder)
-        self._persist_caches_task.defer()
-        return res
-
-    @cached_task(cache_class=DownloadTaskCache)
     async def download_file_contents(self, studip_file, local_dest=None, chunk_size=1024 * 256):
         # FIXME add explicit calls to circuit breaker to individual download parts, potentially allowing retrying failed parts
         if not local_dest:
@@ -124,7 +120,7 @@ class CachedStudIPSession(StudIPSession):
 
         if await self.has_cached_download(studip_file, local_dest):
             log.info("Re-using existing file for download %s -> %s", studip_file, local_dest)
-            download = Download(self.ahttp, self._get_download_url(studip_file), local_dest, chunk_size)
+            download = Download(self.ahttp, self.get_download_url(studip_file), local_dest, chunk_size)
             await download.load_completed()
             return download
 
