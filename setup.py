b254ee2b182d7627905471dd688bc7ed3bca269c
Niko Fink <nfink95@gmail.com>
2018-04-24 20:45:20 +0200

    add missing dependency for async-timeout

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
             "fusepy",
             "argparse",
             "appdirs",
+            "async-timeout",
             # "sh",
             "pyyaml",
             "frozendict",
