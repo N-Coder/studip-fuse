7e134cc1213742b5d62b9e93e9778974360af0dc
Niko Fink <nfink95@gmail.com>
2018-06-06 13:22:12 +0200

    add separate representation of file information to studip_path
    and proper segment_needs_expand_loop checking / handling

--- studip_fuse/studipfs/path/studip_path.py
+++ studip_fuse/studipfs/path/studip_path.py
@@ -1,11 +1,11 @@
 import asyncio
-import itertools
 import logging
 import os
+import re
+import warnings
 from asyncio import Queue
 from datetime import datetime
 from enum import IntEnum
-from os import path
 from stat import S_IFDIR, S_IFREG, S_IRGRP, S_IROTH, S_IRUSR
 from typing import Any, Dict, Optional, Set, Tuple
 
@@ -13,7 +13,7 @@ import attr
 from async_generator import async_generator, yield_
 from cached_property import cached_property
 
-from studip_fuse.avfs.path_util import path_head, path_tail
+from studip_fuse.avfs.path_util import join_path, path_head, path_name
 from studip_fuse.avfs.virtual_path import FormatToken, VirtualPath, get_format_str_fields
 from studip_fuse.studipfs.api.downloader import Download
 from studip_fuse.studipfs.api.session import StudIPSession
@@ -71,12 +71,14 @@ class Pipeline(object):
 class DataField(IntEnum):
     Semester = 1
     Course = 2
-    File = 3
+    Folder = 3
+    File = 4
 
 
-File = DataField.File
-Course = DataField.Course
 Semester = DataField.Semester
+Course = DataField.Course
+Folder = DataField.Folder
+File = DataField.File
 
 
 @attr.s(frozen=True, str=False, repr=False, hash=False)
@@ -89,7 +91,8 @@ class StudIPPath(VirtualPath):
             raise ValueError("Invalid keys for known_data: %s" % inv_keys)
 
         if not self.is_folder:
-            assert self._file, \
+            assert self._folder and self._folder.get("folder_type", None) and \
+                   self._file and self._file.get("mime_type", None), \
                 "Virtual path %s has no more possible path segments (and thus must be a file, " \
                 "not a folder), but doesn't uniquely describe a single file. " \
                 "Does your path format specification make sense?" % self
@@ -105,12 +108,7 @@ class StudIPPath(VirtualPath):
         has = max(self.known_data.keys(), default=0)
         wants = max(self.content_options, default=0)
 
-        if has == wants == DataField.File and self.segment_needs_expand_loop and self._file.is_folder:
-            # loop over contents of one folder
-            folder, subfolders, files = self.session.get_folder_details(self._file)
-            for data in itertools.chain(subfolders, files):
-                await yield_(self._mk_sub_path(new_known_data=data, increment_path_segments=False))
-        elif wants <= has:
+        if wants <= has and not self.segment_needs_expand_loop:
             # we already know all we want to know
             await yield_(self._mk_sub_path())
         else:
@@ -120,11 +118,16 @@ class StudIPPath(VirtualPath):
                 pipeline.add_processor(self.__list_semesters)
             if Course in needs:
                 pipeline.add_processor(self.__list_courses)
-            if File in needs:
-                pipeline.add_processor(self.__list_root_file)
+            if Folder in needs:
+                pipeline.add_processor(self.__get_root_folder)
+            if File in needs or self.segment_needs_expand_loop:
+                pipeline.add_processor(self.__list_folder_contents)
+                pipeline.add_processor(self.__inflate_file_ids)
             pipeline.put(self.known_data)
             async for data in pipeline.drain():
-                await yield_(self._mk_sub_path(new_known_data=data))
+                if not isinstance(data, VirtualPath):
+                    data = self._mk_sub_path(new_known_data=data)
+                await yield_(data)
 
     async def __list_semesters(self, item, out_queue):
         async for semester in self.session.get_semesters():
@@ -135,16 +138,38 @@ class StudIPPath(VirtualPath):
         async for course in self.session.get_courses(semester):
             out_queue.put_nowait({Semester: semester, Course: course})
 
-    async def __list_root_file(self, item, out_queue):
+    async def __get_root_folder(self, item, out_queue):
         semester = item[Semester]
         course = item[Course]
-        folder, subfolders, files = await self.session.get_course_root_file(course)
-        out_queue.put_nowait({Semester: semester, Course: course, File: folder})
+        folder, subfolders, files = await self.session.get_course_root_folder(course)
+        out_queue.put_nowait({Semester: semester, Course: course, Folder: folder})
 
-    def getattr(self):
+    async def __list_folder_contents(self, item, out_queue):
+        folder, subfolders, files = await self.session.get_folder_details(item[Folder])
+        for subfolderid in subfolders:
+            out_queue.put_nowait({**item, Folder: subfolderid})
+        for fileid in files:
+            out_queue.put_nowait({**item, File: fileid})
+
+    async def __inflate_file_ids(self, item, out_queue):
+        if File in item and isinstance(item[File], str):
+            file = await self.session.get_file_details(item[File])
+            out_queue.put_nowait({**item, File: file})
+        elif Folder in item and isinstance(item[Folder], str):
+            subfolder, subsubfolders, subfiles = await self.session.get_folder_details(item[Folder])
+            if not subfolder.get("is_visible", False) or not subfolder.get("is_readable", False):
+                warnings.warn("Ignoring non-readable folder with id %s: %s" % (item[Folder], subfolder))
+            else:
+                # we need to set increment_path_segments=False, so put full VirtualPath object instead of data dict into queue
+                out_queue.put_nowait(self._mk_sub_path(
+                    new_known_data={**item, Folder: subfolder},
+                    increment_path_segments=False))
+
+    async def getattr(self):
         d = dict(st_ino=hash(self.partial_path), st_nlink=1,
                  st_mode=S_IFDIR if self.is_folder else S_IFREG)
-        if self.is_folder or self._file.is_accessible:
+        # TODO update
+        if self.is_folder or self._folder.is_accessible:
             d["st_mode"] |= S_IRUSR | S_IRGRP | S_IROTH
         if hasattr(os, "getuid"):
             d["st_uid"] = os.getuid()
@@ -155,11 +180,23 @@ class StudIPPath(VirtualPath):
         if self.mod_times[1]:
             d["st_mtime"] = self.mod_times[1].timestamp()
         if not self.is_folder:
-            if self._file.size is None:
-                log.warning("Size of file %s unknown, because the value wasn't loaded from Stud.IP", self._file)
+            if self._folder.size is None:
+                log.warning("Size of file %s unknown, because the value wasn't loaded from Stud.IP", self._folder)
             else:
-                d["st_size"] = self._file.size
+                d["st_size"] = self._folder.size
         return d
+
+    async def getxattrs(self):
+        # TODO implement based on extended known_tokens
+        # "folder-id": self.__escape(self._folder["id"]),  # '3c90ca04794bce6661f985c664a5d6cd'
+        # "folder-name": self.__escape(self._folder["name"]),  # 'Virtuelle Maschinen und Laufzeitsysteme'
+        # "folder-description": self.__escape(self._folder["description"]),  # ''
+        # "folder-type": self.__escape(self._folder["folder_type"]),  # 'RootFolder'
+        # "author-id": self.__escape(self._folder["user_id"]),  # 'cli'
+        # "is_visible": self.__escape(self._folder["is_visible"]),  # True
+        # "is_readable": self.__escape(self._folder["is_readable"]),  # True
+        # "is_writable": self.__escape(self._folder["is_writable"]),  # True
+        return {}
 
     async def open_file(self, flags) -> Download:
         assert not self.is_folder, "open_file called on folder %s" % self
@@ -169,108 +206,128 @@ class StudIPPath(VirtualPath):
 
     @cached_property
     def content_options(self) -> Set[DataField]:
-        format_segment = path_head(self.next_path_segments)
         requirements = set()
-        for field_name in get_format_str_fields(format_segment):
-            if field_name in ["semester", "semester-lexical", "semester-lexical-short"]:
-                requirements.add(Semester)
-            elif field_name in ["course", "course-abbrev", "course-id", "type", "type-abbrev"]:
-                requirements.add(Course)
-            elif field_name in ["path", "short-path", "id", "name", "description", "author"]:
-                requirements.add(File)
-            elif field_name == "time" and not requirements:  # any info can provide a time
-                requirements.add(Semester)
-            else:
-                raise ValueError("Unknown format field name '%s' in format string '%s'" % (field_name, format_segment))
+        if self.next_path_segments:
+            format_segment = path_head(self.next_path_segments)
+            for field_name in get_format_str_fields(format_segment):
+                if field_name in ["semester-id", "semester", "semester-short", "semester-lexical", "semester-lexical-short"]:
+                    requirements.add(Semester)
+                elif field_name in ["course-id", "course-number", "course", "course-subtitle", "course-description",
+                                    "course-abbrev", "course-type", "course-class", "course-type-abbrev",
+                                    "course-location", "course-grouping"]:
+                    requirements.add(Course)
+                elif field_name in ["path", "short-path"]:
+                    requirements.add(Folder)
+                elif field_name in ["file-id", "file-name", "file-description", "file-size", "file-mime-type",
+                                    "file-terms", "file-storage", "file-downloads"]:
+                    requirements.add(File)
+                else:
+                    raise ValueError("Unknown format field name '%s' in format string '%s'" % (field_name, format_segment))
         return requirements
 
     @cached_property
     def segment_needs_expand_loop(self) -> bool:
-        return self.is_folder and any(t in path_head(self.next_path_segments) for t in ["{path}", "{short-path}"])
+        return self.path_segments and any(t in path_name(self.path_segments) for t in ["{path}", "{short-path}"])
 
     @cached_property
     def known_tokens(self) -> Dict[FormatToken, Any]:
-        tokens = {  # TODO time may differ between file and parent folder, which will break path logic (see issue #2)
-            "created": self.mod_times[0],
-            "changed": self.mod_times[1],
+        path, short_path = self.known_folder_path
+        tokens = {
+            "path": self.__escape_path(path),
+            "short-path": self.__escape_path(short_path),
         }
+        # TODO add abbrev algorithms from python StudIP-API
         if self._semester:
             tokens.update({
-                "semester": self.__escape_file(self._semester.name),
-                "semester-lexical": self.__escape_file(self._semester.lexical),
-                "semester-lexical-short": self.__escape_file(self._semester.lexical_short),
+                "semester-id": self._semester["id"],  # '4cb8438b3057e71a627ab7e25d73ba75'
+                "semester": self.__escape(self._semester["description"]),  # 'Wintersemester 2017/2018'
+                "semester-short": self.__escape(self._semester["title"]),  # 'WS 17/18'
+                # "semester-lexical": self.__escape(self._semester.lexical),
+                # "semester-lexical-short": self.__escape(self._semester.lexical_short),
             })
-
         if self._course:
+            number = re.sub("[^0-9]", "", self._course["number"])
+            type_abbrev = re.sub("[0-9]", "", self._course["number"])
             tokens.update({
-                "semester": self.__escape_file(self._course.semester.name),
-                "semester-lexical": self.__escape_file(self._course.semester.lexical),
-                "semester-lexical-short": self.__escape_file(self._course.semester.lexical_short),
-
-                "course-id": self._course.id,
-                "course-abbrev": self.__escape_file(self._course.abbrev),
-                "course": self.__escape_file(self._course.name),
-                "type": self.__escape_file(self._course.type),
-                "type-abbrev": self.__escape_file(self._course.type_abbrev),
+                "course-id": self._course["course_id"],  # '00093e6878c6c7733579251567a177da'
+                "course-number": self.__escape(number),  # '5795'
+                "course": self.__escape(self._course["title"]),  # 'Virtuelle Maschinen und Laufzeitsysteme'
+                "course-subtitle": self.__escape(self._course["subtitle"]),  # ''
+                "course-description": self.__escape(self._course["description"]),  # ''
+                # "course-abbrev": self.__escape(abbrev),
+                "course-type": self.__escape(self._course["type"]),  # 'Uebung'
+                "course-class": self.__escape(self._course["class"]),  # 'Lehre'
+                "course-type-abbrev": self.__escape(type_abbrev),
+                "course-location": self.__escape(self._course["location"]),  # ''
+                "course-grouping": self.__escape(self._course["group"]),  # 1
             })
-
         if self._file:
-            path = short_path = self._file.path[1:-1]  # '1234VL-Name'/[path]/'file_name'
-
-            # skip "Allgemeiner Dateiordner" if it is the only object in course root dir / has no siblings
-            root_file = self._file
-            while root_file.parent and root_file.parent.parent:
-                root_file = root_file.parent
-            if root_file.name in ["Allgemeiner Dateiordner", "Hauptordner"] and root_file.is_single_child:
-                short_path = path_tail(short_path)
-
             tokens.update({
-                "semester": self.__escape_file(self._file.course.semester.name),
-                "semester-lexical": self.__escape_file(self._file.course.semester.lexical),
-                "semester-lexical-short": self.__escape_file(self._file.course.semester.lexical_short),
-
-                "course-id": self._file.course.id,
-                "course-abbrev": self.__escape_file(self._file.course.abbrev),
-                "course": self.__escape_file(self._file.course.name),
-                "type": self.__escape_file(self._file.course.type),
-                "type-abbrev": self.__escape_file(self._file.course.type_abbrev),
-
-                "path": self.__escape_path(path),
-                "short-path": self.__escape_path(short_path),
-
-                "id": self._file.id,
-                "name": self.__escape_file(self._file.name),
-                "description": self.__escape_file(self._file.description or ""),
-                "author": self.__escape_file(self._file.author or ""),
+                "file-id": self.__escape(self._file["id"]),  # '3c90ca04794bce6661f985c664a5d6cd'
+                "file-name": self.__escape(self._file["name"]),  # 'Virtuelle Maschinen und Laufzeitsysteme'
+                "file-description": self.__escape(self._file["description"]),  # ''
+                "file-size": self.__escape(self._file["size"]),  # '118738',
+                "file-mime-type": self.__escape(self._file["mime_type"]),  # 'application/pdf',
+                "file-terms": self.__escape(self._file["content_terms_of_use_id"]),  # 'SELFMADE_NONPUB',
+                "file-storage": self.__escape(self._file["storage"]),  # 'disk',
+                "file-downloads": self.__escape(self._file["downloads"]),  # '118',
             })
         return tokens
 
+    @cached_property
+    def known_folder_path(self) -> Tuple[str, str]:
+        path = short_path = []
+        vp = self
+        visited_folders = []
+        while vp and vp._folder:
+            if vp._folder not in visited_folders:
+                if visited_folders:
+                    # we are walking the hierarchy up, so the next upper folder should be the parent of the lower folder
+                    assert vp._folder["id"] == visited_folders[-1]["parent_id"]
+                visited_folders.append(vp._folder)
+                path = [vp._folder["name"]] + path
+                if vp._folder.get("folder_type", None) != "RootFolder":
+                    if vp._folder["name"] in ["Allgemeiner Dateiordner", "Hauptordner", self._course["title"] if self._course else None]:
+                        warnings.warn("Folder has name %s indicating a root folder, but type is %s: %s" % (
+                            vp._folder["name"], vp._folder["folder_type"], vp._folder))
+                    short_path = [vp._folder["name"]] + short_path
+
+            assert vp is not vp.parent
+            vp = vp.parent
+
+        return path, short_path
+
     # utils  ###########################################################################################################
 
-    def __escape_file(self, str):
+    def __escape(self, str):
         return escape_file_name(str, Charset.Ascii, EscapeMode.Similar)
 
     def __escape_path(self, folders):
-        return path.join(*map(self.__escape_file, folders)) if folders else ""
+        return join_path(*map(self.__escape, folders)) if folders else ""
 
     @cached_property
-    def _file(self) -> File:
+    def _file(self):
         return self.known_data.get(File, None)
 
     @cached_property
-    def _course(self) -> Course:
+    def _folder(self):
+        return self.known_data.get(Folder, None)
+
+    @cached_property
+    def _course(self):
         return self.known_data.get(Course, None)
 
     @cached_property
-    def _semester(self) -> Semester:
+    def _semester(self):
         return self.known_data.get(Semester, None)
 
     @cached_property
     def mod_times(self) -> Tuple[Optional[datetime], Optional[datetime]]:
-        if self._file:
-            return self._file.created, self._file.changed
+        if self._folder:
+            return self._folder["mkdate"], self._folder["chdate"]
         if self._course:
-            return (self._course.semester.start_date,) * 2
+            return (self._course.semester["start_date"],) * 2
         if self._semester:
-            return (self._semester.start_date,) * 2
+            # "begin", "end", "seminars_begin", "seminars_end"
+            return (datetime.fromtimestamp(self._semester["begin"]),) * 2
         return None, None
